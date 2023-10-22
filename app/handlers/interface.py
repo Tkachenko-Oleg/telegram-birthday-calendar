7ad66479342bcf7d91d1062b037176f1d3f33324
Tkachenko-Oleg <tkachenkoolegd@gmail.com>
2023-10-22 11:40:13 +0300

    Update. Interface class

---
+++ app/handlers/interface.py
@@ -0,0 +1,32 @@
+class BotCommandLogic:
+    """class bot command logic"""
+    def get_all_birthdays(self, db: dict):
+        """
+        Get all birthdays from database
+
+        :param db: database with all birthdays
+        :return: list with all birthdays
+        """
+        pass
+
+    def get_today_birthdays(self, db: dict):
+        """
+        Get all birthdays whose date is equal to today's date
+
+        :param db: database with all birthdays
+        :return: list with all birthdays whose date is equal to today's date
+        """
+        pass
+
+    def add_new_birthday(self, db: dict, name: str, year: int, month: int, day: int):
+        """
+        Add a new birthday to the database
+
+        :param db: database with birthdays
+        :param name: username added to the database
+        :param year: year of birthday
+        :param month: month of birthday
+        :param day: day of birthday
+        :return: None
+        """
+        pass
