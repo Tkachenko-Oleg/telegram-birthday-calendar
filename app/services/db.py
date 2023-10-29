69576e246761c7dca20f8a8fb8bbce4a87248adb
Tkachenko-Oleg <tkachenkoolegd@gmail.com>
2023-10-29 11:26:39 +0300

    Update.

--- app/services/db.py
+++ app/services/db.py
@@ -5,9 +5,9 @@ from dataclasses import dataclass
 @dataclass
 class Data:
     data = {
-        0: {'user_id': '', 'name': 'Oleg', 'birthday': {'year': '2007', 'month': '12', 'day': '31'}},
-        1: {'user_id': '', 'name': 'Vika', 'birthday': {'year': '2007', 'month': '03', 'day': '29'}},
-        2: {'user_id': '', 'name': 'Maxim', 'birthday': {'year': '2007', 'month': '10', 'day': '08'}},
+        0: {'user_id': '2064313437', 'name': 'Oleg', 'birthday': {'year': '2007', 'month': '12', 'day': '31'}},
+        1: {'user_id': '2064313437', 'name': 'Vika', 'birthday': {'year': '2007', 'month': '03', 'day': '29'}},
+        2: {'user_id': '2064313437', 'name': 'Maxim', 'birthday': {'year': '2007', 'month': '10', 'day': '08'}},
         3: {'user_id': '', 'name': 'Ivan', 'birthday': {'year': '2001', 'month': '06', 'day': '13'}},
         4: {'user_id': '', 'name': 'Vlad', 'birthday': {'year': '1998', 'month': '01', 'day': '03'}},
         5: {'user_id': '', 'name': 'Nikita', 'birthday': {'year': '2013', 'month': '12', 'day': '01'}},
